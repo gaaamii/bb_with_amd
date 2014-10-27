04f18035e318a0b0d72cece30b7402e0f42f0944
gaaamii <i1shi4ga4mi1@gmail.com>
2014-10-27 17:41:29 +0900

    thanks @karur4n

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 (function() {
-  console.log("Backbone");
-  console.log(Backbone);
-  console.log("jQuery");
-  console.log($);
-  console.log("Underscore");
-  console.log(_);
+  requirejs.config({
+  // pathsオプションの設定。"module/name": "path"を指定します。拡張子（.js）は指定しません。
+    paths: {
+      'Backbone': 'lib/backbone',
+      'jquery': 'lib/jquery',
+      'underscore': 'lib/underscore'
+      }
+    });
+  define(["Backbone"], function(Backbone) {
+    console.log(Backbone);
+  });
 })();
